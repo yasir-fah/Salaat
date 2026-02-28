72b43ac83eba67572947d3a3ae315b7690a01697
dev <dev@test.com>
2026-03-01 00:01:13 +0300

    fix syntax

--- src/main/java/com/finalproject/tuwaiqfinal/Config/SwaggerConfig.java
+++ src/main/java/com/finalproject/tuwaiqfinal/Config/SwaggerConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
